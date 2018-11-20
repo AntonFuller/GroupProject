15c0d9422885e6c8f811fa18825770f59877fdb4
Anton Manikaew-Fuller (s5118771) <s5118771@imac-7.local>
2018-11-20 10:25:01 +0000

    Initial Commit

---
+++ GroupProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GroupProject
+//
+//  Created by Anton Manikaew-Fuller (s5118771) on 20/11/2018.
+//  Copyright © 2018 (s5118771). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
