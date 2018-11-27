83d2e73ad12c9b09b013beb3dfd2a326a0831bfd
Anton Manikaew-Fuller (s5118771) <s5118771@mac38574.student.bournemouth.ac.uk>
2018-11-27 10:36:45 +0000

    added UIKit

--- GroupProject/AlcoholDataBase.swift
+++ GroupProject/AlcoholDataBase.swift
@@ -1,12 +1,5 @@
-//
-//  AlcoholDataBase.swift
-//  GroupProject
-//
-//  Created by Anton Manikaew-Fuller (s5118771) on 27/11/2018.
-//  Copyright © 2018 (s5118771). All rights reserved.
-//
-
 import Foundation
+import UIKit
 
 
 class AlcoholDataBase: UIViewController {
