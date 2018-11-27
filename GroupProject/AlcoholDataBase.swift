af0b14d16f503ddc73866e8173ea4b7056b4bd24
Anton Manikaew-Fuller (s5118771) <s5118771@mac38574.student.bournemouth.ac.uk>
2018-11-27 12:53:41 +0000

    changing background
    trying to change the background so we have two gradient colours fading together

--- GroupProject/AlcoholDataBase.swift
+++ GroupProject/AlcoholDataBase.swift
@@ -4,6 +4,17 @@ import UIKit
 
 class AlcoholDataBase: UIViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    let layer = CAGradientLayer()
+    layer.frame = view.bounds
+    layer.colors = [UIColor.yellow.cgColor, UIColor.green.cgColor]
+        view.layer.addSublayer(layer)
+        layer.startPoint = CGPoint(x: 0, y: 0)
+        layer.stopPoint = CGPoint (x: 1, y: 1)
+        view.layer.addSublayer(layer)
+        
     
     
     
@@ -11,5 +22,5 @@ class AlcoholDataBase: UIViewController {
     
     
     
-    
+   }
 }
