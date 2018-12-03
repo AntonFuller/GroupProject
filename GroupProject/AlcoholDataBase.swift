6de1af749e4be357a977973fa3b7ea05d9653056
Anton Manikaew-Fuller (s5118771) <s5118771@mac42946.student.bournemouth.ac.uk>
2018-12-03 14:06:17 +0000

    install pods

--- GroupProject/AlcoholDataBase.swift
+++ GroupProject/AlcoholDataBase.swift
@@ -7,13 +7,15 @@ class AlcoholDataBase: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     
-    let layer = CAGradientLayer()
-    layer.frame = view.bounds
-    layer.colors = [UIColor.yellow.cgColor, UIColor.green.cgColor]
-        view.layer.addSublayer(layer)
-        layer.startPoint = CGPoint(x: 0, y: 0)
-        layer.stopPoint = CGPoint (x: 1, y: 1)
-        view.layer.addSublayer(layer)
+        //changing the colour of the background to two gradients
+        
+//    let layer = CAGradientLayer()
+//    layer.frame = view.bounds
+//    layer.colors = [UIColor.yellow.cgColor, UIColor.green.cgColor]
+//        view.layer.addSublayer(layer)
+//        layer.startPoint = CGPoint(x: 0, y: 0)
+//        layer.stopPoint = CGPoint (x: 1, y: 1)
+//        view.layer.addSublayer(layer)
         
     
     
---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '11.0'
+
+target 'GroupProject' do
+
+  use_frameworks!
+
+  pod 'Firebase/Core'
+  pod 'Firebase/Firestore'
+
+end
