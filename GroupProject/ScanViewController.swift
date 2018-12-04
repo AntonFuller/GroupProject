6f1c7f9b213014cc306b11738af5a35597d79da8
SuffTheStudent <43778888+SuffTheStudent@users.noreply.github.com>
2018-12-04 09:38:12 +0000

    Added Capture Data

--- GroupProject/ScanViewController.swift
+++ GroupProject/ScanViewController.swift
@@ -2,7 +2,12 @@ import UIKit
 import AVFoundation
 
 class SwiftViewController: UIViewController {
- 
+    class BarcodeReaderViewController: UIController, AVCaptureMetadataOutputObjectsDelegate {
+        var session: AVCaptureSession
+        var previewLayer: AVCaptureVideoPreviewLayer!
+    }
+    
+    
     
 }
 
