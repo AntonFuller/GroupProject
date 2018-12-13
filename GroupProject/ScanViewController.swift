8b82209f0a791aca9ff2726c68eab5dab50fa447
SuffTheStudent <43778888+SuffTheStudent@users.noreply.github.com>
2018-12-13 13:35:15 +0000

    Added a Camera view

--- GroupProject/ScanViewController.swift
+++ GroupProject/ScanViewController.swift
@@ -1,36 +1,44 @@
 import UIKit
 import AVFoundation
 
-class SwiftViewController: UIViewController {
-    class BarcodeReaderViewController: UIController, AVCaptureMetadataOutputObjectsDelegate {
-        var captureSession: AVCaptureSession!
-        var previewLayer: AVCaptureVideoPreviewLayer!
-        
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            
-            view.backgroundColor = UIColor.black
-            captureSession = AVCaptureSession()
-            
-            guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
-            let videoInput: AVCaptureDeviceInput
-            
-            do {
-                videoInput = try AVCaptureDevice(device: videoCaptureDevice)
-            } catch {
-                return
-            }
-            if (captureSession.canAddInput(videoInput)) {
-                captureSession.AddInput(videoInput)
-            } else {
-                failed()
-                return
-            }
+class CameraView: UIView {
+    override class var layerClass: AnyClass {
+        get {
+            return AVCaptureVideoPreviewLayer.self
         }
     }
-    
-    
+    class SwiftViewController: UIViewController {
+        class BarcodeReaderViewController: UIController, AVCaptureMetadataOutputObjectsDelegate {
+            var captureSession: AVCaptureSession!
+            var previewLayer: AVCaptureVideoPreviewLayer!
+            
+            
+            override func viewDidLoad() {
+                super.viewDidLoad()
+                
+                view.backgroundColor = UIColor.black
+                captureSession = AVCaptureSession()
+                
+                guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+                let videoInput: AVCaptureDeviceInput
+                
+                do {
+                    videoInput = try AVCaptureDevice(device: videoCaptureDevice)
+                } catch {
+                    return
+                }
+                if (captureSession.canAddInput(videoInput)) {
+                    captureSession.AddInput(videoInput)
+                } else {
+                    failed()
+                    return
+                }
+            }
+        }
+        
+        
+        
+        
+    }
     
 }
-
