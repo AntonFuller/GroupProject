22e75d2aa60d8e438034441ce308ecc9810d8fec
Hannah Mason (s5107757) <s5107757@mac42934.student.bournemouth.ac.uk>
2019-01-07 13:46:20 +0000

    shops again

--- GroupProject/ShopManager.swift
+++ GroupProject/ShopManager.swift
@@ -7,11 +7,16 @@ class ShopManager {
     
     init() {
         
-        let tescoCoordinate = CLLocationCoordinate2D(latitude: 50.7259162,  longitude: -1.8651469
-        let tesco = Shop(coordinate: tescoCoordinate, title: "sfkgjhdf")
+        let tescoCoordinate = CLLocationCoordinate2D(latitude: 50.7259162,  longitude: -1.8651469)
+        let tesco = Shop(coordinate: tescoCoordinate, title: "Tesco")
         shops.append(tesco)
-
         
-    }
+    
+
+        let sainsburrysCoordinate = CLLocationCoordinate2D(latitude: 50.7268498,  longitude: -1.8408748)
+        let sainsburrys = Shop(coordinate: sainsburrysCoordinate, title: "Sainsburrys")
+        shops.append(sainsburrys)
     
 }
+}
+
