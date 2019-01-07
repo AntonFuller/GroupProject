340ca0ff317b8b8c5a7edc4408b471632ba74ae5
Hannah Mason (s5107757) <s5107757@mac42934.student.bournemouth.ac.uk>
2019-01-07 14:06:28 +0000

    g

--- GroupProject/ScanViewController.swift
+++ GroupProject/ScanViewController.swift
@@ -9,6 +9,52 @@ class CameraView: UIView {
     }
     class SwiftViewController: UIViewController {
         class BarcodeReaderViewController: UIController, AVCaptureMetadataOutputObjectsDelegate {
+            func isEqual(_ object: Any?) -> Bool {
+                <#code#>
+            }
+            
+            var hash: Int
+            
+            var superclass: AnyClass?
+            
+            func `self`() -> Self {
+                <#code#>
+            }
+            
+            func perform(_ aSelector: Selector!) -> Unmanaged<AnyObject>! {
+                <#code#>
+            }
+            
+            func perform(_ aSelector: Selector!, with object: Any!) -> Unmanaged<AnyObject>! {
+                <#code#>
+            }
+            
+            func perform(_ aSelector: Selector!, with object1: Any!, with object2: Any!) -> Unmanaged<AnyObject>! {
+                <#code#>
+            }
+            
+            func isProxy() -> Bool {
+                <#code#>
+            }
+            
+            func isKind(of aClass: AnyClass) -> Bool {
+                <#code#>
+            }
+            
+            func isMember(of aClass: AnyClass) -> Bool {
+                <#code#>
+            }
+            
+            func conforms(to aProtocol: Protocol) -> Bool {
+                <#code#>
+            }
+            
+            func responds(to aSelector: Selector!) -> Bool {
+                <#code#>
+            }
+            
+            var description: String
+            
             var captureSession: AVCaptureSession!
             var previewLayer: AVCaptureVideoPreviewLayer!
             
