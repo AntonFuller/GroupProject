a2985cd40de3120ef57c63e35f853f9d474c4680
Hannah Mason (s5107757) <s5107757@mac42934.student.bournemouth.ac.uk>
2019-01-07 14:01:46 +0000

    waitrose

--- GroupProject/ShopManager.swift
+++ GroupProject/ShopManager.swift
@@ -16,7 +16,22 @@ class ShopManager {
         let sainsburrysCoordinate = CLLocationCoordinate2D(latitude: 50.7268498,  longitude: -1.8408748)
         let sainsburrys = Shop(coordinate: sainsburrysCoordinate, title: "Sainsburrys")
         shops.append(sainsburrys)
-    
+        
+        let asdaCoordinate = CLLocationCoordinate2D(latitude: 50.7527593 ,  longitude: -1.8401066)
+        let asda = Shop(coordinate: asdaCoordinate, title: "Asda")
+        shops.append(asda)
+        
+        let waitroseCoordinate = CLLocationCoordinate2D(latitude: 50.7268733 ,  longitude: -1.9745883)
+        let waitrose = Shop(coordinate: waitroseCoordinate, title: "Asda")
+        shops.append(waitrose)
+        
+        let coopCoordinate = CLLocationCoordinate2D(latitude: 50.7213915 ,  longitude: -1.869695)
+        let coop = Shop(coordinate: coopCoordinate, title: "Asda")
+        shops.append(coop)
+        
+        
+    }
 }
-}
+
+
 
