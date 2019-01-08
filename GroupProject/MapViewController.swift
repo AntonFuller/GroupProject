b7b8d3eee7c2505b5f3d2e0d9a409d942d92eafe
Hannah Mason (s5107757) <s5107757@mac38575.student.bournemouth.ac.uk>
2019-01-08 09:17:06 +0000

    Resolved conflict

--- GroupProject/MapViewController.swift
+++ GroupProject/MapViewController.swift
@@ -5,13 +5,6 @@ import CoreLocation
 class MapViewController: UIViewController{
     
     @IBOutlet weak var MapView: MKMapView!
-<<<<<<< HEAD
-    let locationManager = CLLocationManager()
-   
-    
-=======
->>>>>>> 3a860a62fd3e5a2154eb4a9a21db2baff6e81f84
-    
     let locationManager = CLLocationManager()
     let shopManger = ShopManager()
     
